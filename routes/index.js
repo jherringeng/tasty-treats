6b4d4b87fbd527126c315f9769e1cb15353d06a1
dirtyCount <dirtyCount@gmail.com>
2021-03-06 22:27:11 +0000

    Bug fixes and improvements

--- routes/index.js
+++ routes/index.js
@@ -18,15 +18,16 @@ router.post('/', function (req, res) {
     var now = new Date();
     var nowString = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() + "-" + now.getHours() + "-" + now.getMinutes();
     req.body['dateTime'] = nowString;
+    if ("subscribed" in req.body) {
+      req.body['subscribed'] = "Subscribed";
+    } else {
+      req.body['subscribed'] = "Not Subscribed";
+    }
     var messageData = JSON.stringify(req.body);
 
     var messageFileName = 'message-' + nowString + '.txt';
     console.log(messageFileName);
 
-    // fs.open(messageFileName, 'w', function (err, file) {
-    //   if (err) throw err;
-    //   console.log('Saved!');
-    // });
 
     fs.writeFile('./messages/'+ messageFileName, messageData, function(err) {
     if(err) {
@@ -35,91 +36,88 @@ router.post('/', function (req, res) {
     console.log("The file was saved!");
     });
 
-    // Or
-    // fs.writeFileSync('/tmp/test-sync', 'Hey there!');
-
     res.redirect('/thank-you');
 });
 
-router.get('/messages', function(req, res, next) {
+// router.get('/messages', function(req, res, next) {
+//
+//   var messagesData = ['Why', 'not', 'working?'];
+//
+//   fs.readdir('./messages', (err, files) => {
+//     files.forEach(file => {
+//       console.log(file);
+//       fs.readFile('./messages/' + file, 'utf8' , (err, data) => {
+//         if (err) {
+//           console.error(err)
+//           return
+//         }
+//         messagesData.push(data)
+//         console.log(messagesData)
+//       })
+//     });
+//   })
+//
+//   messagesData.push('test')
+//   console.log(messagesData);
+//   res.render('messages', { title: 'Your messages', messagesData: messagesData });
+// });
+//
+// var messageFiles=['message.txt'], messageData = ['somedata'];
+//
+// router.get('/messages1', async function(req, res, next) {
+//   await getMessageFiles()
+//   await getMessageDataFromFile()
+//
+//   res.render('messages1', { title: 'Your messages', messageData: messageData, messageFiles: messageFiles });
+// })
+//
+// async function getMessageFiles() {
+// 	try {
+// 	   await fs.readdir('./messages', (err, files) => {
+//         files.forEach(file => {
+//           console.log('filename ' + file);
+//           messageFiles.push(file);
+//         })
+//     });
+//     console.log(messageFiles);
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// }
+//
+// async function getMessageData() {
+//   console.log("get message data");
+// 	try {
+//     await messageFiles.forEach(async function(file) {
+//       console.log(file);
+//       await getMessageDataFromFile(file)
+//       // messageData.push("data")
+//     })
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// }
+//
+// async function getMessageDataFromFile() {
+// 	try {
+//     messageFiles.forEach(async function(file) {
+//       console.log('./messages/' + file);
+//       await fs.readFile('./messages/' + file, 'utf8' , function (err, data) {
+//          if (err) {
+//            console.error('Error: ' + err)
+//          }
+//          console.log('data output ' + data);
+//          messageData.push(data)
+//          console.log('message data ' + messageData);
+//        })
+//      })
+//     // messageData.push("data")
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// }
 
-  var messagesData = ['Why', 'not', 'working?'];
-
-  fs.readdir('./messages', (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-      fs.readFile('./messages/' + file, 'utf8' , (err, data) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        messagesData.push(data)
-        console.log(messagesData)
-      })
-    });
-  })
-
-  messagesData.push('test')
-  console.log(messagesData);
-  res.render('messages', { title: 'Your messages', messagesData: messagesData });
-});
-
-var messageFiles=['message.txt'], messageData = ['somedata'];
-
-router.get('/messages1', async function(req, res, next) {
-  await getMessageFiles()
-  await getMessageDataFromFile()
-
-  res.render('messages1', { title: 'Your messages', messageData: messageData, messageFiles: messageFiles });
-})
-
-async function getMessageFiles() {
-	try {
-	   await fs.readdir('./messages', (err, files) => {
-        files.forEach(file => {
-          console.log('filename ' + file);
-          messageFiles.push(file);
-        })
-    });
-    console.log(messageFiles);
-	} catch (err) {
-		console.log(err);
-	}
-}
-
-async function getMessageData() {
-  console.log("get message data");
-	try {
-    await messageFiles.forEach(async function(file) {
-      console.log(file);
-      await getMessageDataFromFile(file)
-      // messageData.push("data")
-    })
-	} catch (err) {
-		console.log(err);
-	}
-}
-
-async function getMessageDataFromFile() {
-	try {
-    messageFiles.forEach(async function(file) {
-      console.log('./messages/' + file);
-      await fs.readFile('./messages/' + file, 'utf8' , function (err, data) {
-         if (err) {
-           console.error('Error: ' + err)
-         }
-         console.log('data output ' + data);
-         messageData.push(data)
-         console.log('message data ' + messageData);
-       })
-     })
-    // messageData.push("data")
-	} catch (err) {
-		console.log(err);
-	}
-}
-
-router.get('/test', async function(req, res, next) {
+router.get('/messages', async function(req, res, next) {
   var testDataArray = [];
   var messageDataJSONArray = [];
   var testFileNames = await testDirRead();
@@ -130,9 +128,9 @@ router.get('/test', async function(req, res, next) {
     var messageDataJSON = JSON.parse(contents);
     messageDataJSONArray.push(messageDataJSON)
   }
-  // var testData = await testFileRead("message.txt");
+  messageDataJSONArray.sort( compareDates );
 
-  res.render('test', { title: 'Your messages', messageDataJSONArray: messageDataJSONArray, testFileNames: testFileNames, testDataArray: testDataArray });
+  res.render('messages', { title: 'Your messages', messageDataJSONArray: messageDataJSONArray, testFileNames: testFileNames, testDataArray: testDataArray });
 })
 
 async function testDirRead() {
@@ -146,6 +144,16 @@ async function testDirRead() {
 async function testFileRead(file) {
     const data = await fsp.readFile("./messages/" + file, "binary");
     return new Buffer(data);
+}
+
+function compareDates( a, b ) {
+  if ( a.dateTime > b.dateTime ){
+    return -1;
+  }
+  if ( a.dateTime < b.dateTime ){
+    return 1;
+  }
+  return 0;
 }
 
 module.exports = router;
